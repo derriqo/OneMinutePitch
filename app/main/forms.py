9e5480b4e646a0f8bb09675facd5e90299a08b35
derrrick william <williamderrick100@gmail.com>
2019-02-12 11:40:08 +0300

    Removed unnecesary files and also request functions.

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+# class ReviewForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
-    submit = SubmitField('Submit')
+#     title = StringField('Review title',validators=[Required()])
+#     review = TextAreaField('Movie review')
+#     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
