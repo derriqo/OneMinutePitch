7e64e813fff11458d56629a9b5dc7c1f22b1d504
derrrick william <williamderrick100@gmail.com>
2019-02-14 10:55:20 +0300

    Further additions.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY='fd3b82e0e765b17e63b600ed3f42b6ea'
+# export MOVIE_API_KEY='fd3b82e0e765b17e63b600ed3f42b6ea'
 export SECRET_KEY='0987'
 
 export MAIL_USERNAME='williamderrickcode@gmail.com'
