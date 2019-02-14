d0c343691f8069f7cf35bcb0eaadf887ce14839e
derrrick william <williamderrick100@gmail.com>
2019-02-14 11:26:42 +0300

    Deployment.

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User,Role
 from  flask_migrate import Migrate, MigrateCommand
 
 #creating app
-app = create_app("development")
+app = create_app("production")
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command("server",Server)
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-# export MOVIE_API_KEY='fd3b82e0e765b17e63b600ed3f42b6ea'
 export SECRET_KEY='0987'
 
 export MAIL_USERNAME='williamderrickcode@gmail.com'
